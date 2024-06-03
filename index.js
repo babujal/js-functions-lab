881b320b5b4a8014e1d65faafe6396327278400c
babujal <dercamar1@gmail.com>
2024-06-02 22:47:24 -0400

    Copleted

--- index.js
+++ index.js
@@ -205,7 +205,16 @@ Example: basicCalculator(10, 5, 'subtract') should return 5.
 Complete the exercise in the space below:
 */
 const basicCalculator = (numOne, numTwo, operation) => {
-    
+    if (operation == 'add') {
+        return numOne + numTwo
+    }else if (operation == 'subtract') {
+        return numOne - numTwo
+    }else if (operation == 'multiply') {
+        return numOne * numTwo
+    }else if (operation == 'divide') {
+        return numOne / numTwo
+    }
+    return 'Wrong operation, please type one of the follow (add, subtract, multiply, divide)'
 }
 
 
